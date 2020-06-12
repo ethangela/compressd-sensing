fb20be1cbc98195bd19d01a0492eb602405c385a
ethangela <malcolmsun@foxmail.com>
2020-06-12 14:22:28 +0800

    Add files via upload

--- src/compressed_sensing_for_image_signal.py
+++ src/compressed_sensing_for_image_signal.py
@@ -1,6 +1,6 @@
 """Compressed sensing main script"""
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0" ###########################################
+os.environ["CUDA_VISIBLE_DEVICES"] = "0" 
 import numpy as np
 import tensorflow as tf
 print('Tensorflow version', tf.__version__)
@@ -19,10 +19,18 @@ def _pad(x, kernel_size, pad):
     else:
         return x
 
-def _upsample(x, upsample_mode):
-    if upsample_mode == 'none':
-        return x
-    
+def _upsample(x, upsample_mode): ####################################
+    if upsample_mode == '1D':
+        w = tf.shape(x)[1]
+        h = tf.shape(x)[2]
+        new_shape = tf.stack([w * 2, h], axis=0)
+        try:
+            # align_corners = True necessary for bilinear interpolation
+            x = tf.image.resize_images(x, new_shape, align_corners=True,
+                                       method=getattr(tf.image.ResizeMethod, upsample_mode.upper()))
+            return x
+        except AttributeError:
+            raise NotImplementedError('%s rescaling' % upsample_mode)
     else:
         w = tf.shape(x)[1]
         h = tf.shape(x)[2]
@@ -90,6 +98,57 @@ def decodernw(inputs,
         net = tf.nn.sigmoid(net)    
     return net #Outputs a 4D Tensor (batch, width*2^6, height*2^6, channels=3) 
 
+def decodernw_1D(inputs,
+              num_output_channels=1, 
+              num_channels_up=[128] * 5,
+              filter_size_up=1,
+              need_sigmoid=True, 
+              pad='reflection',
+              upsample_mode='bilinear', 
+              act_fun=tf.nn.relu, # tf.nn.leaky_relu 
+              bn_before_act=False,
+              bn_affine=True,
+              upsample_first=True
+             ):
+    """Deep Decoder.
+       Takes as inputs a 4D Tensor (batch, width, height, channels)""" #which is the fixed noise with channels = 
+    ## Configure
+    num_channels_up = num_channels_up[1:] + [num_channels_up[-1], num_channels_up[-1]] #[128, 128 ,...] with shape of [1,6]
+    n_scales = len(num_channels_up) #equals to 6
+    
+    if not (isinstance(filter_size_up, list) or isinstance(filter_size_up, tuple)) :
+        filter_size_up = [filter_size_up] * n_scales #[1, 1 ,...] with shape of [1,6]                                               
+        
+    ## Deep Decoder
+    net = inputs
+    for i, (num_channels, kernel_size) in enumerate(zip(num_channels_up, filter_size_up)):       
+        # Upsample (first)
+        if upsample_first and i != 0:
+            net = _upsample(net, upsample_mode)
+
+        # Conv        
+        net = _pad(net, kernel_size, pad)
+        net = tf.layers.conv2d(net, num_channels, kernel_size=kernel_size, strides=1, activation=None, padding='valid', use_bias=False)
+
+        # Upsample (second)
+        if not upsample_first and i != len(num_channels_up) - 1:
+            net = _upsample(net, upsample_mode)
+
+        # Batch Norm + activation
+        if bn_before_act: 
+            net = _bn(net, bn_affine)           
+        net = act_fun(net)
+        if not bn_before_act: 
+            net = _bn(net, bn_affine) 
+                
+    # Final convolution
+    kernel_size = 1
+    net = _pad(net, kernel_size, pad)
+    net = tf.layers.conv2d(net, num_output_channels, kernel_size, strides=1, 
+                           activation=None, padding='valid', use_bias=False)
+    if need_sigmoid:
+        net = tf.nn.sigmoid(net)    
+    return net #Outputs a 4D Tensor (batch, width*2^6, height*2^6, channels=3) 
 
 # == fit part == #
 def get_num_params():
