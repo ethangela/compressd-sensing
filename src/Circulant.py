72cf47c19411708628838a156266d35aa75e16a1
ethangela <malcolmsun@foxmail.com>
2020-10-10 14:49:13 +0800

    Add files via upload

--- src/Circulant.py
+++ src/Circulant.py
@@ -5,7 +5,7 @@ import numpy as np
 from scipy import linalg
 
 
-
+#Scipy
 def conventional_circulant(signal_vector, t):
     #signal length
     n = len(signal_vector)
@@ -35,7 +35,7 @@ def fft_circulant(signal_vector, t):
     r1 = ifft(signal_vector)
     
     #step 2: Diag() @ F^{-1} @ x
-    Ft = np.sqrt(n) * fft(t)
+    Ft = n * fft(t) #was np.sqrt(n)
     r2 = np.multiply(r1, Ft)
     
     #step 3: F @ Diag() @ F^{-1} @ x
@@ -43,12 +43,46 @@ def fft_circulant(signal_vector, t):
     
     return compressive
 
+
+#Tensorflow
+def tf_fft_circulant(signal_vector, signal_size, t): 
+    signal_vector = tf.convert_to_tensor(signal_vector, dtype=tf.complex64)
+    t = tf.convert_to_tensor(t, dtype=tf.complex64)
+    
+    #signal length
+    tf_n = tf.constant(signal_size, dtype=tf.complex64)
+    
+    #step 1: F^{-1} @ x
+    r1 = tf.signal.ifft(signal_vector)
+    
+    #step 2: Diag() @ F^{-1} @ x
+    Ft = tf_n * tf.signal.fft(t) #was tf.math.sqrt(n)
+    r2 = tf.multiply(r1, Ft)
+    
+    #step 3: F @ Diag() @ F^{-1} @ x
+    compressive = tf.signal.fft(r2)
+    float_compressive = tf.cast(compressive, tf.float32)
+    
+    return float_compressive
+
+
+#PyTorch
+def torch_fft_circulant(signal_vector, signal_size, t): 
+    #step 1: F^{-1} @ x
+    signal_vector = signal_vector.unsqueeze(-1)
+    signal_vector = torch.cat((signal_vector, torch.zeros(signal_vector.size)), -1)
+    r1 = torch.ifft(signal_vector, 1)
+    
+    #step 2: Diag() @ F^{-1} @ x
+    Ft = signal_size * torch.fft(t, 1, onesided=False) #was np.sqrt(signal_size)
+    r2 = torch.mul(r1,Ft)
+     
+    #step 3: F @ Diag() @ F^{-1} @ x
+    compressive = torch.fft(r2, 1)
+    
+    return compressive #NOTE: this is a complex value, not a real value, need drop the last dimension for subsequent calculations 
+
 if __name__ == "__main__":
     signal_vector = np.array([1,2,3,4,5,6,7,8,9])
-    t = np.random.normal(size=len(signal_vector))   
-    r1 = conventional_circulant(signal_vector, t)
-    r2 = fft_circulant(signal_vector, t)
-    
-    print(r1)
-    print('\t')
-    print(r2)
+    t = np.random.normal(size=len(signal_vector))
+    signal_size = 8
