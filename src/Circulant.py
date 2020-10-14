a682e581e761559382b2be7b06620323c009a12f
ethangela <malcolmsun@foxmail.com>
2020-10-14 16:38:10 +0800

    Add files via upload

--- src/Circulant.py
+++ src/Circulant.py
@@ -1,8 +1,9 @@
 from scipy.linalg import dft
-from scipy.fftpack import fft, ifft
+from scipy.fft import fft, ifft
 from scipy.sparse import diags
 import numpy as np
 from scipy import linalg
+from scipy.linalg import norm
 
 
 #Scipy
@@ -11,7 +12,7 @@ def conventional_circulant(signal_vector, t):
     n = len(signal_vector)
     
     #Fourier Matrix
-    F = dft(n) #should be with 'sqrtn', however we choose to ignore the normlisation in order to align with fft()
+    F = dft(n, scale='sqrtn') 
 
     #Diagonal
     dig = diags(np.sqrt(n) * F.dot(t)).toarray()
@@ -26,63 +27,67 @@ def conventional_circulant(signal_vector, t):
     return compressive
   
 
-  
-def fft_circulant(signal_vector, t): 
+def fft_circulant(signal_vector, t, test=False): 
     #signal length
     n = len(signal_vector)
     
     #step 1: F^{-1} @ x
-    r1 = ifft(signal_vector)
-    
+    if not test:
+        r1 = ifft(signal_vector, norm="ortho")
+    else:
+        r1 = ifft(signal_vector)
     #step 2: Diag() @ F^{-1} @ x
-    Ft = n * fft(t) #was np.sqrt(n)
+    if not test: 
+        Ft = np.sqrt(n) * fft(t, norm="ortho")
+    else:
+        Ft = fft(t)
     r2 = np.multiply(r1, Ft)
     
     #step 3: F @ Diag() @ F^{-1} @ x
-    compressive = fft(r2)
+    if not test:
+        compressive = fft(r2, norm="ortho")
+    else:   
+        compressive = fft(r2)
     
     return compressive
 
 
 #Tensorflow
-def tf_fft_circulant(signal_vector, signal_size, t): 
+def circulant_tf(signal_vector, t, signal_size):  
     signal_vector = tf.convert_to_tensor(signal_vector, dtype=tf.complex64)
     t = tf.convert_to_tensor(t, dtype=tf.complex64)
     
-    #signal length
-    tf_n = tf.constant(signal_size, dtype=tf.complex64)
-    
     #step 1: F^{-1} @ x
-    r1 = tf.signal.ifft(signal_vector)
+    r1 = tf.signal.ifft(signal_vector, name='circulant_step1_ifft')               
     
     #step 2: Diag() @ F^{-1} @ x
-    Ft = tf_n * tf.signal.fft(t) #was tf.math.sqrt(n)
-    r2 = tf.multiply(r1, Ft)
+    Ft = tf.signal.fft(t)
+    r2 = tf.multiply(r1, Ft, name='circulant_step2_diag')                
     
     #step 3: F @ Diag() @ F^{-1} @ x
-    compressive = tf.signal.fft(r2)
-    float_compressive = tf.cast(compressive, tf.float32)
-    
-    return float_compressive
+    compressive = tf.signal.fft(r2, name='circulant_step3_fft')
+                         
+    with tf.Session() as sess:
+        print(sess.run(compressive))
+    sess.close()
 
 
-#PyTorch
-def torch_fft_circulant(signal_vector, signal_size, t): 
-    #step 1: F^{-1} @ x
-    signal_vector = signal_vector.unsqueeze(-1)
-    signal_vector = torch.cat((signal_vector, torch.zeros(signal_vector.size)), -1)
-    r1 = torch.ifft(signal_vector, 1)
-    
-    #step 2: Diag() @ F^{-1} @ x
-    Ft = signal_size * torch.fft(t, 1, onesided=False) #was np.sqrt(signal_size)
-    r2 = torch.mul(r1,Ft)
-     
-    #step 3: F @ Diag() @ F^{-1} @ x
-    compressive = torch.fft(r2, 1)
-    
-    return compressive #NOTE: this is a complex value, not a real value, need drop the last dimension for subsequent calculations 
 
 if __name__ == "__main__":
-    signal_vector = np.array([1,2,3,4,5,6,7,8,9])
-    t = np.random.normal(size=len(signal_vector))
-    signal_size = 8
+    import tensorflow as tf
+    
+    signal_vector = np.array([1,2,3,4,5,6,7,8,9,10])
+    t = np.random.normal(size=len(signal_vector))   
+    r1 = conventional_circulant(signal_vector, t)
+    r2 = fft_circulant(signal_vector, t)
+    r3 = fft_circulant(signal_vector, t, test=True)
+    
+    print(r1)
+    print('\t')
+    print(r2)
+    print('\t')
+    print(r3)
+    print('\t')
+    circulant_tf(signal_vector,t,10)
+    
+    
