da24035943d696c65146cdbbe4f28aaf528eb5d7
ethangela <malcolmsun@foxmail.com>
2020-12-14 12:51:08 +0800

    Add files via upload

--- src/Gaussian_1D.py
+++ src/Gaussian_1D.py
@@ -1,32 +1,26 @@
 import numpy as np 
-import numpy.matlib
-from matplotlib import pyplot as plt
-import scipy.stats as st
-from scipy.stats import truncnorm
-from skimage.io import imread, imsave
-from skimage.color import rgb2gray, rgba2rgb, gray2rgb
-import numpy as np
-import matplotlib .pyplot as plt
 from scipy.stats import multivariate_normal
-from matplotlib import cm
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-from scipy.optimize import minimize
+from argparse import ArgumentParser
+from scipy.special import gamma, kv
 
+def matern_kernel(r, l = 1, v = 1):
+    r[r == 0] = 1e-8
+    part1 = 2 ** (1 - v) / gamma(v)
+    part2 = (np.sqrt(2 * v) * r / l) ** v
+    part3 = kv(v, np.sqrt(2 * v) * r / l)
+    return part1 * part2 * part3
 
-def sample_from_1D_Gaussian(size, kernel, l_val):
+def sample_from_1D_Gaussian(size, kernel, l_val, idx):
     
     #sample points range 
-    m = size*size
-    xs = np. linspace (-10,10,m) #Range vector (101,)
+    m = size  
+    xs = np.linspace(-10,10,m) #Range vector (101,)
     
     #mean vetor 
     mxs = np.zeros(m) #Zero mean vector (101,)
 
-    #rbf kernel
-    l = l_val #hyperparameters #2, sqrt(0.5), 1
+    #covariance by kernel
+    l = l_val #hyperparameters, i.e. 2, sqrt(0.5), 1
     sigma_square = 1 #hyperparameters
     if kernel == 'rbf':
         Kss = sigma_square * np.exp( -1/(l**2) * np.abs(xs[:,np.newaxis]-xs[:,np.newaxis].T)**2 ) #Covariance matrix
@@ -34,16 +28,18 @@ def sample_from_1D_Gaussian(size, kernel, l_val):
         Kss = np.exp( -1/l * np.abs(xs[:,np.newaxis]-xs[:,np.newaxis].T) ) #Covariance matrix
     elif kernel == 'peri':
         Kss = np.exp( -2/(l**2) * np.sin(np.abs(xs[:,np.newaxis]-xs[:,np.newaxis].T)/2)**2 ) #Covariance matrix
-    print('done 0')
+    elif kernel == 'matern':
+        Kss = matern_kernel(r=np.abs(xs[:,np.newaxis]-xs[:,np.newaxis].T), l=l)
+    print('step 0 complete')
     
     #sample 
     s = 10 #number of drawing samples from the prior
-    fs = multivariate_normal(mean=mxs ,cov=Kss , allow_singular =True).rvs(s).T
-    print('done 1')
+    fs = multivariate_normal(mean=mxs ,cov=Kss , allow_singular=True).rvs(s).T
+    print('step 1 complete')
     print(fs.shape)
     r = np.random.randint(s, size=1)
     fs = fs[:, r[0]]
-    print('done 2')
+    print('step 2 complete')
     
     #scale to range(0,1)
     xs = (xs - np.min(xs))/np.ptp(xs)
@@ -51,14 +47,22 @@ def sample_from_1D_Gaussian(size, kernel, l_val):
     print(fs.shape)
     
     #save 
-    plt.plot(xs, fs, 'gray') # Plot the samples
-    #plt.title('l = sqrt(2)')
-    plt.savefig('1D_'+kernel+'_'+str(l_val)+'.jpg')
-    np.save('1D_'+kernel+'_'+str(l_val)+'.npy', fs)
+    ##plt.plot(xs, fs, 'gray') # Plot the samples
+    ##plt.savefig('1D_'+kernel+'_'+str(l_val)+'.jpg')
+    #np.save('1D_'+kernel+'_'+str(l_val)+'.npy', fs)
+    np.save('image/Gaussian signal/1D_'+kernel+'_'+str(l_val)+'_'+str(size)+'_'+str(idx)+'.npy', fs)
    
-
+   
+def main(size, hparams, idx):
+    return sample_from_1D_Gaussian(size, hparams.kernel, hparams.length, idx) 
 
 
 if __name__ == '__main__':
-    #gaussian_2D_special_case(64)
-    sample_from_1D_Gaussian(64, 'rbf', 2)
+    
+    size_list = [int(32*pow(p, l)) for p in [1.5, 2, 3, 4] for l in [2,3,4,5]][:-1]
+    
+    for num in range(1,6):
+        for i in range(len(size_list)):
+            for kernel in [('exp',0.25), ('matern',1.0), ('rbf',3.0)]:
+                sample_from_1D_Gaussian(size_list[i], kernel[0], kernel[1], num)
+    
--- src/Gaussian_2D.py
+++ src/Gaussian_2D.py
@@ -1,9 +1,10 @@
 import numpy as np
-from matplotlib import pyplot as plt
+#from matplotlib import pyplot as plt
 from scipy.stats import multivariate_normal
-from matplotlib import cm
+#from matplotlib import cm
+from argparse import ArgumentParser
 
-def kernel(x1, x2, ker, l=2, sigma_f=1):#################################################
+def kernel(x1, x2, ker, l=3, sigma_f=1):
     dist_matrix = np.sum(x1**2, 1).reshape(-1, 1) + np.sum(x2**2, 1) - 2 * np.dot(x1, x2.T)
     d = np.sqrt(dist_matrix)
     if ker == 'rbf':
@@ -15,7 +16,7 @@ def kernel(x1, x2, ker, l=2, sigma_f=1):########################################
         conv = np.exp(-2 / (l**2) * np.sin(d/2)**2)
     return conv
 
-def sample_from_2D_Gaussian(size, ker):
+def sample_from_2D_Gaussian(size, ker, l, idx):
     m = size
     test_d1 = np.linspace(-10, 10, m)
     test_d2 = np.linspace(-10, 10, m)
@@ -24,7 +25,7 @@ def sample_from_2D_Gaussian(size, ker):
     test_X = [[d1, d2] for d1, d2 in zip(test_d1.ravel(), test_d2.ravel())]
     test_X = np.asarray(test_X)
     mu = np.zeros_like(test_d1)
-    cov = kernel(test_X, test_X, ker)
+    cov = kernel(test_X, test_X, ker, l)
     print('parameter set done')
     
     #gp_samples = np.random.multivariate_normal(mean=mu.ravel(), cov = cov, size = 1)
@@ -35,20 +36,30 @@ def sample_from_2D_Gaussian(size, ker):
     
     #scale to range(0,1)
     z = (z - np.min(z))/np.ptp(z)
-    name = '2D_' + ker + '.npy'
+    name = 'image/Gaussian signal/2D_' + ker +'_'+ str(l) +'_'+ str(size) +'_'+ str(idx) + '.npy'
     np.save(name, z)
     #print(z)
-    test_d1 = (test_d1 - np.min(test_d1))/np.ptp(test_d1)
-    test_d2 = (test_d2 - np.min(test_d2))/np.ptp(test_d2)
-    print('scaling done')
     
-    fig = plt.figure(figsize=(5, 5))
-    plt.contourf(test_d1, test_d2, z, cmap=cm.coolwarm, alpha=1)
-    #ax.set_title("with optimization l=%.2f sigma_f=%.2f" % (gpr.params["l"], gpr.params["sigma_f"]))
-    #plt.show()
-    img_name = '2D_' + ker + '.jpg'
-    plt.savefig(img_name)
+    ##test_d1 = (test_d1 - np.min(test_d1))/np.ptp(test_d1)
+    ##test_d2 = (test_d2 - np.min(test_d2))/np.ptp(test_d2)
+    ##print('scaling done')
+    ##fig = plt.figure(figsize=(5, 5))
+    ##plt.contourf(test_d1, test_d2, z, cmap=cm.coolwarm, alpha=1)
+    ##img_name = '2D_' + ker + '_' + str(l) + '.jpg'
+    ##plt.savefig(img_name)
     
     
-if __name__ == '__main__':    
-    sample_from_2D_Gaussian(64, 'exp')
+def main(hparams):
+    return sample_from_2D_Gaussian(hparams.size, hparams.kernel, hparams.length, 1) 
+
+
+if __name__ == '__main__':
+
+    PARSER = ArgumentParser()
+    PARSER.add_argument('--size', type=int, default=256, help='size')
+    PARSER.add_argument('--kernel', type=str, default='rbf', help='kernel')
+    PARSER.add_argument('--length', type=float, default=3.0, help='length')
+    
+    HPARAMS = PARSER.parse_args()
+    
+    main(HPARAMS)
